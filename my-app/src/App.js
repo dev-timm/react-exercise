cf4b9b1032677051c2aff2a33d86a5ade7e68874
dev-timm <timm.kuemmeth@gmail.com>
2023-11-03 14:38:57 +0000

    add event handling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,13 @@ import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProp
 import StatefulGreeting from './components/StatefulGreeting';
 import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import EventsFunctional from './components/EventsFunctional';
+import EventsClass from './components/EventsClass';
+import EventBinding from './components/EventBinding';
+import ConditionalRenderingFunctional from './components/ConditonalRenderingFunctional';
+import ConditionalRenderingClass from './components/ConditonalRenderingClass';
+import NestingComponents from './components/NestingComponents';
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
 
 function App() {
   return (
@@ -11,7 +18,15 @@ function App() {
       {/* <FunctionalGreetingWithProps greeting="nice to meet you!!" name="Mike" age="32" /> */}
       {/* <StatefulGreeting greeting="I'm a stateful class component" name="Mike" /> */}
       {/* <StatefulGreetingWithCallback greeting="I'm a stateful class component" name="Mike" /> */}
-      <StatefulGreetingWithPrevState greeting="I'm a stateful class component" name="Mike" />
+      {/* <StatefulGreetingWithPrevState greeting="I'm a stateful class component" name="Mike" /> */}
+      {/* <EventsFunctional /> */}
+      {/* <EventsClass /> */}
+      {/* <EventBinding /> */}
+      {/* <ConditionalRenderingFunctional connected={false} /> */}
+      {/* <ConditionalRenderingClass /> */}
+      {/* <NestingComponents /> */}
+      <MethodsAsPropsParent />
+
       
     </div>
   );
